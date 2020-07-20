1846a830b78290dec2ddf4a08f4ef83e2e27c5e5
GPrince00 <gustavoprince0@gmail.com>
2020-07-20 20:59:35 -0300

    feat(home): creates the home and adds the styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import './App.css';
 
-import Header from './Header';
+import Home from './pages/home';
 
 function App() {
   return (
-    <div>
-      <Header title="Hello World" />
-      <h1>Conteúdo da aplicação</h1>
-    </div>
+    <Home />
   );
 }
 
